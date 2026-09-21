76d1ef1127d9035880c96170dd45dd94d68ca989
te24oscfle <te24oscfle@falufri.se>
2026-09-21 17:20:53 +0200

    Created CommandRegistry

---
+++ Backend/CommandSystem/CommandRegistry.cs
@@ -0,0 +1,47 @@
+using System.Reflection;
+
+namespace MusicPlayerApp
+{
+    public static class CommandRegistry
+    {
+        public static Dictionary<string, CommandDef> DiscoverCommands()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // Discover all commands in project and create command definitions.
+            var commandDefs = assembly
+                .GetTypes()
+                .Where(type =>
+                    typeof(ICommand).IsAssignableFrom(type) &&
+                    type is { IsClass: true, IsAbstract: false})
+                .Select(type => new
+                {
+                    Type = type,
+                    Attribute = type.GetCustomAttribute<CommandAttribute>()
+                })
+                .Where(x => x.Attribute != null)
+                .Select(x => new CommandDef(
+                    (ICommand)Activator.CreateInstance(x.Type)!,
+                    x.Attribute!)
+                );
+            
+            // Add aliases
+            var allCommands = new Dictionary<string, CommandDef>();
+
+            foreach (var commandDef in commandDefs)
+            {
+                allCommands.Add(commandDef.Metadata.Name, commandDef);
+                
+                if (commandDef.Metadata.Aliases == null)
+                    continue;
+                
+                foreach (string alias in commandDef.Metadata.Aliases)
+                {
+                    allCommands.Add(alias, commandDef);    
+                }
+            }
+            
+            return allCommands;
+        }
+    }
+}
