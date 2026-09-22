bca7e76e581d76f90fdb14c4c8f27ab7d4b82e48
te24oscfle <te24oscfle@falufri.se>
2026-09-22 20:32:31 +0200

    Created Track class

--- Backend/Library/Models/Track.cs
+++ Backend/Library/Models/Track.cs
@@ -2,6 +2,13 @@ namespace MusicPlayerApp
 {
     public class Track
     {
-        
+        public int TrackId;
+        public string FilePath;
+
+        public Track(int trackId, string filePath)
+        {
+            TrackId = trackId;
+            FilePath = filePath;
+        }
     }
 }
